0979ac3ce84d8897e86cd94f23fe2904f8b937ae
YoungwanCho <developer119korea@gmail.com>
2021-02-24 21:46:48 +0900

    페이지네이션에서 마지막 페이지를 출력

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 
-print(indeed_result.text)
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+links = pagination.find_all('a')
+pages = []
+for link in links[:-1]:
+  pages.append(int(link.string))
+
+max_page = pages[-1]
+
+print(max_page)
