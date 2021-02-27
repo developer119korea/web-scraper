079b9b9d54aab35bd0b6b004c4a75da0e2989bcd
Youngwan Cho <developer119korea@gmail.com>
2021-02-27 07:43:56 +0000

    장소와 링크를 추가하여 구직 정보를 넘겨준다

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from indeed import extract_indeed_pages, extract_indeed_jobs
 
 last_indeed_page = extract_indeed_pages()
 
-extract_indeed_jobs(last_indeed_page)
+jobs = extract_indeed_jobs(last_indeed_page)
+
+print(jobs)
